1aa1d9d0fd1e64686fde76b78d9d459f56caabcf
MELBOURNE <melbourne@SS-BUSINESS-PRO.local>
2025-01-18 00:26:10 +1100

    included custom Error handling

--- ecart/backend/routes/productRoutes.js
+++ ecart/backend/routes/productRoutes.js
@@ -16,9 +16,10 @@ router.get('/:id', asyncHandler(async(req, res) => {
 
   if(product){
     res.json(product);
-  }
-
-  res.status(404).json({message: 'Product not found'});
+  }else{
+    res.status(404);
+    throw new Error('Resource not found');
+  } 
 }));
 
 export default router;
